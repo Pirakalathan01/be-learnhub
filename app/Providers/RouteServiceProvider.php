f5be10eef0f6594825e0973e8c6079927ce60424
pirakalathan <pirakalathanpilla99@gmail.com>
2024-04-04 01:32:32 +0530

    base repository add

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,12 +29,18 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+        });
+
+        Route::group([
+            'middleware' => 'api',
+            'prefix' => 'api',
+        ], function () {
+            $routes = glob(base_path('routes/api/*.php'));
+            foreach ($routes as $route) {
+                require $route;
+            }
         });
     }
 }
